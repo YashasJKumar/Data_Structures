f151ebd615c37376e01df8ab89d8b1526046b485
Yashas J Kumar <yashasjkumar6@gmail.com>
2024-10-07 20:54:12 +0530

    Demonstration of Diamond Problem in C++

---
+++ OOPS/diamond.cpp
@@ -0,0 +1,44 @@
+#include "iostream"
+
+using namespace std;
+
+class Animal
+{
+public:
+    void eat(){
+        cout<<"Animal is eating."<<endl;
+    }
+};
+
+class Dog : virtual public Animal
+{
+//public:
+//    void eat(){
+//        cout<<"Dog is eating."<<endl;
+//    }
+};
+
+class Cat :virtual public Animal
+{
+public:
+    void eat(){
+        cout<<"Cat is eating."<<endl;
+    }
+};
+
+class Mixed : public Dog, public Cat
+{
+//public:
+//    void eat(){
+//        cout<<"Mixed Eating."<<endl;
+//    }
+};
+
+int main()
+{
+    Mixed m;
+    m.eat();
+//    m.Cat::eat();  // Avoiding ambiguity through Scope Resolution.
+
+//    m.eat();
+}
