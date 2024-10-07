414e5014a758f568f0829530fc369b607a86c2c0
Yashas J Kumar <yashasjkumar6@gmail.com>
2024-10-07 20:49:05 +0530

    Updated the problems on Hashing.

--- Hashing/problems.cpp
+++ Hashing/problems.cpp
@@ -161,6 +161,50 @@ int longest_subarray_with_equal_01(int arr[], int n)
     return cur_largest + 1;
 }
 
+int longest_subarray_with_01_efficient(int arr[], int n)
+{
+    int prefix_sum = 0, ans = 0;
+    unordered_map<int , int> map;
+
+    for(int i = 0; i < n;i++)
+    {
+        if(!arr[i])
+            prefix_sum += -1;
+        else
+            prefix_sum += arr[i];
+
+        if(prefix_sum == 0)
+            ans = i + 1;
+
+        if(map.find(prefix_sum) == map.end())
+            map.insert(make_pair(prefix_sum, i));
+        else
+            ans = max(ans, i - map[prefix_sum]);
+    }
+    return ans;
+}
+
+int longest_subarray_with_sum(int arr[], int n, int sum)
+{
+    unordered_map<int, int> hash_map;
+    int prefix_sum = 0, ans = 0;
+
+    for(int i = 0; i < n;i++)
+    {
+        prefix_sum += arr[i];
+
+        if(prefix_sum == sum)
+            ans = i + 1; //This is to handle the special case => where prefix sum starting from index 0.
+
+        if(hash_map.find(prefix_sum) == hash_map.end())
+            hash_map.insert(make_pair(prefix_sum, i));
+
+        if(hash_map.find(prefix_sum - sum) != hash_map.end())
+            ans = max(ans, i - hash_map[prefix_sum - sum]);
+    }
+    return ans;
+}
+
 int longest_common_subarray(int arr1[], int arr2[], int n)
 {
     unordered_map<int, int> map;
@@ -170,22 +214,71 @@ int longest_common_subarray(int arr1[], int arr2[], int n)
     int max_len = 0, sum = 0;
     for (int i = 0; i < n; i++)
     {
-        // Add current element to sum 
+        // Add current element to sum
         sum += diff[i];
 
-        // To handle sum=0 at last index 
+        // To handle sum=0 at last index
         if (sum == 0)
             max_len = i + 1;
 
-        // If this sum is seen before, 
-        // then update max_len if required 
+        // If this sum is seen before,
+        // then update max_len if required
         if (map.find(sum) != map.end())
             max_len = max(max_len, i - map[sum]);
 
-        else // Else put this sum in hash table 
+        else // Else put this sum in hash table
             map[sum] = i;
     }
     return max_len;
+}
+
+int lcs_of_array(int arr[], int n)
+{
+    int cur = 1, res = 1;
+    unordered_set<int> hash_table(arr, arr + n);
+
+    for(int i = 0; i < n;i++)
+    {
+        if(hash_table.find(arr[i] - 1) == hash_table.end()) {
+            cur = 1;
+
+            while(hash_table.find(arr[i] + cur) != hash_table.end())
+                cur++;
+            res = max(res, cur);
+        }
+    }
+    return res;
+}
+
+void distinct_elements_in_every_window(int arr[], int n, int k)
+{
+    //k -> Size of the window & k < n.
+    if(k > n)
+        return;
+
+    unordered_map<int, int> frequency_map(k);
+
+    //Insert the elements of first window into the map.
+    for(int i = 0; i < k;i++)
+        frequency_map[arr[i]]++;
+
+    cout<<frequency_map.size()<<" ";
+
+    for(int i = k; i < n;i++)
+    {
+        frequency_map[arr[i-k]]--;
+
+        if(!frequency_map[arr[i-k]])
+            frequency_map.erase(arr[i-k]);
+        if(frequency_map.find(arr[i]) != frequency_map.end())
+            frequency_map[arr[i]]++;
+        else
+            frequency_map[arr[i]] = 1;
+
+        cout<<frequency_map.size()<<" ";
+    }
+
+    cout<<endl;
 }
 
 int main()
@@ -207,4 +300,12 @@ int main()
     subarray_with_sum_0(arr, n) ? cout<<"Sub-Array Exists with Sum = 0"<<endl : cout<<"Sub-Array Doesn't Exist with Sum=0"<<endl;
     subarray_with_given_sum(arr, n, 22) ? cout<<"Sub-Array Exists with Sum = 22"<<endl : cout<<"Sub-Array Doesn't Exist with Sum=22"<<endl;
     cout<<"Largest sub-array with equal no. of 0's & 1's is: "<<longest_subarray_with_equal_01(arr, n)<<endl;
+
+    int bin_arr[] = {1, 0, 1,1,1,0,0};
+    cout<<"Length of the Longest subarray with equal 0 & 1 => "<<longest_subarray_with_01_efficient(bin_arr, 7)<<endl;
+
+    cout<<"LCS -> "<<lcs_of_array(arr, n)<<endl;
+
+    int arr3[] = {10,20,20,10,30,40,10};
+    distinct_elements_in_every_window(arr3, 7, 4);
 }
