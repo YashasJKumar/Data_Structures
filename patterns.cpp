bf587475fee8868f9c5ee345779fdca517a934b1
Yashas J Kumar <yashasjkumar6@gmail.com>
2025-01-25 18:09:44 +0530

    Uploaded pattern problems

--- patterns.cpp
+++ patterns.cpp
@@ -1,6 +1,27 @@
 #include "iostream"
 using namespace std;
 
+void pyramid(int n) {
+    for (int i = 1;i <= n;i++) {
+        //Spaces
+        for (int j = 1;j <= n - i;j++)
+            cout<<"  ";
+        //Print *'s
+        for (int j = 1;j <= (2 * i - 1);j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < i;j++)
+            cout<<"  ";
+        for (int j = 1;j <= (2 * (n - i) - 1);j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
 int main() {
     int n;
     cout<<"Enter n value: ";
@@ -195,6 +216,9 @@ int main() {
         }
     }
     cout<<endl;
+
+    pyramid(n);
+    return 0;
 }
 
 
