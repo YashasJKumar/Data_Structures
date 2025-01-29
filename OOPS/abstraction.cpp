ff0198beee9d11144ab99f76c6eebb8ff6f51374
Yashas J Kumar <yashasjkumar6@gmail.com>
2025-01-29 22:04:21 +0530

    Slightly modified the code

--- OOPS/abstraction.cpp
+++ OOPS/abstraction.cpp
@@ -11,21 +11,38 @@ protected:
     int length, height, width;
 
 public:
-    virtual ~Shape() {};
+    Shape(const int l, const int h, const int w) {
+        length = l;
+        height = h;
+        width = w;
+    }
+
+    Shape(const int l, const int w) {
+        length = l;
+        width = w;
+        height = l;
+    }
+
+    virtual ~Shape() = default;
+    /*
+     *A trivial destructor in C++ is a destructor that performs no operations and has a default
+     *implementation provided by the compiler. It means the destructor neither has a body nor any
+     *side effects, and it doesn't call the destructors for non-static data members or base classes.
+     *Trivial destructors allow for certain optimizations, like skipping calls to destructors when
+     *deallocating objects of trivially destructible types.
+     */
     virtual int area() = 0;
     // Pure Virtual Functions.
 
     virtual void display() = 0;
 };
 
-class Rectangle : public Shape
+// 'final' => This is the last class in the inheritance hierarchy. No other class can inherit from this class.
+class Rectangle final : public Shape
 {
 public:
-    Rectangle(int length, int width)
-    {
-        this->length = length;
-        this->width = width;
-    }
+    Rectangle(const int length, const int width): Shape(length, width) {}
+
     int area() override
     {
         return length * width;
@@ -37,18 +54,14 @@ public:
     }
 };
 
-class Triangle : public Shape
+class Triangle final : public Shape
 {
 public:
-    Triangle(int height, int width)
-    {
-        this->width = width;
-        this->height = height;
-    }
+    Triangle(int height, int width): Shape(height, width) {}
 
     int area() override
     {
-        return (int)(0.5 * width * height);
+        return static_cast<int>(0.5 * width * height);
     }
 
     void display() override
